a98c16a4ed93c049b04d5941681f2c0843adcb0f
Dmytro Lisitsyn <dl@madappgang.com>
2016-10-10 10:21:44 +0200

    Added Apple Watch target

---
+++ iOS/Watch Extension/SessionManager.swift
@@ -0,0 +1,21 @@
+//
+//  SessionManager.swift
+//  SmartLog
+//
+//  Created by Dmytro Lisitsyn on 10/10/16.
+//  Copyright © 2016 MadAppGang. All rights reserved.
+//
+
+import Foundation
+import CoreMotion
+
+final class SessionManager {
+    
+    init() {
+        
+    }
+    
+    func a() {
+        
+    }
+}
