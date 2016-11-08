dfcd01d715705c4ffd750ced60d1da867a8ac8cc
Dmytro Lisitsyn <dl@madappgang.com>
2016-11-08 22:05:39 +0200

    Added peripheral characteristics receiving

--- iOS/SmartLog/Managers/PolarManager.swift
+++ iOS/SmartLog/Managers/PolarManager.swift
@@ -13,10 +13,16 @@ final class PolarManager: NSObject {
     
     fileprivate enum ServiceUUID {
         static let heartRate = CBUUID(string: "180D")
+        static let battery = CBUUID(string: "180F")
+        
+        static var all: [CBUUID] {
+            return [ServiceUUID.heartRate, ServiceUUID.battery]
+        }
     }
     
-    private enum CharacteristicUUID {
-        
+    fileprivate enum CharacteristicUUID {
+        static let heartRateMeasurement = CBUUID(string: "2A37")
+        static let batteryLevel = CBUUID(string: "2A19")
     }
     
     fileprivate let loggingService: LoggingService?
@@ -29,9 +35,8 @@ final class PolarManager: NSObject {
         
         super.init()
         
-        centralManager = CBCentralManager(delegate: self, queue: nil)
+        centralManager = CBCentralManager(delegate: self, queue: .global(qos: .utility))
     }
-    
 }
 
 extension PolarManager: WearableService {
@@ -44,62 +49,90 @@ extension PolarManager: WearableService {
 extension PolarManager: CBCentralManagerDelegate {
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        switch central.state {
-        case .poweredOn:
-            loggingService?.log("Bluetooth LE is powered on and ready")
-
-            let servicesUUIDs = [ServiceUUID.heartRate]
-            centralManager.scanForPeripherals(withServices: servicesUUIDs, options: nil)
-            
-        case .poweredOff:
-            loggingService?.log("Bluetooth LE is powered off")
-        case .unsupported:
-            loggingService?.log("Bluetooth LE is unsupported on this platform")
-        case .unauthorized:
-            loggingService?.log("Bluetooth LE state is unauthorized")
-        case .unknown:
-            loggingService?.log("Bluetooth LE state is unknown")
-        case .resetting:
-            loggingService?.log("Bluetooth LE is resseting")
+        if case .poweredOn = central.state {
+            centralManager.scanForPeripherals(withServices: ServiceUUID.all, options: nil)
         }
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        guard let name = advertisementData[CBAdvertisementDataLocalNameKey] as? String else { return }
-        guard name.lowercased().contains("polar") else { return }
-
-        central.stopScan()
+        guard let name = peripheral.name, name.lowercased().contains("polar") else { return }
+        
         central.connect(peripheral, options: nil)
+        self.peripheral = peripheral
     }
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         if let name = peripheral.name {
-            loggingService?.log("Device connected \(name)")
+            loggingService?.log("Device connected: \(name)")
         }
         
-        peripheral.delegate = self
+        central.stopScan()
         
-        self.peripheral = peripheral
+        peripheral.delegate = self
+        peripheral.discoverServices(ServiceUUID.all)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        if let name = peripheral.name {
+            loggingService?.log("Device connection failure: \(name)\n\(error)")
+        }
     }
     
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
         if let name = peripheral.name {
-            loggingService?.log("Device disconnected \(name)")
+            loggingService?.log("Device disconnected: \(name)")
         }
+        
+        self.peripheral = nil
+        centralManager.scanForPeripherals(withServices: ServiceUUID.all, options: nil)
     }
 }
 
 extension PolarManager: CBPeripheralDelegate {
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
         
+        for service in services {
+            let characteristics: [CBUUID]
+            
+            switch service.uuid {
+            case ServiceUUID.heartRate:
+                characteristics = [CharacteristicUUID.heartRateMeasurement]
+            case ServiceUUID.battery:
+                characteristics = [CharacteristicUUID.batteryLevel]
+            default:
+                continue
+            }
+            
+            peripheral.discoverCharacteristics(characteristics, for: service)
+        }
     }
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        
+        guard let characteristics = service.characteristics else { return }
+
+        for characteristic in characteristics {
+            switch (service.uuid, characteristic.uuid) {
+            case (ServiceUUID.battery, CharacteristicUUID.batteryLevel):
+                self.peripheral?.readValue(for: characteristic)
+                fallthrough
+            case (ServiceUUID.heartRate, CharacteristicUUID.heartRateMeasurement):
+                self.peripheral?.setNotifyValue(true, for: characteristic)
+            default:
+                break
+            }
+        }
     }
     
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        
+        switch (characteristic.service.uuid, characteristic.uuid) {
+        case (ServiceUUID.heartRate, CharacteristicUUID.heartRateMeasurement):
+            loggingService?.log("🤗: \(characteristic)")
+        case (ServiceUUID.battery, CharacteristicUUID.batteryLevel):
+            loggingService?.log("💀: \(characteristic)")
+        default:
+            break
+        }
     }
 }
