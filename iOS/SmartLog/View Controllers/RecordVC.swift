b185852040a13463d7fd08b5420de8f3c592687f
Dmytro Lisitsyn <dl@madappgang.com>
2016-11-10 23:42:34 +0200

    Improved session recording controls

--- iOS/SmartLog/View Controllers/RecordVC.swift
+++ iOS/SmartLog/View Controllers/RecordVC.swift
@@ -10,12 +10,28 @@ import UIKit
 
 final class RecordVC: UIViewController {
 
+    private enum State {
+        case recording
+        case paused
+        case stopped
+    }
+    
     @IBOutlet fileprivate weak var heartRateLabel: UILabel!
-    @IBOutlet fileprivate weak var startButton: UIButton!
-    @IBOutlet fileprivate weak var stopButton: UIButton!
+    
+    @IBOutlet private weak var startButton: UIButton!
+    @IBOutlet private weak var pauseButton: UIButton!
+    @IBOutlet private weak var resumeButton: UIButton!
+    @IBOutlet private weak var stopButton: UIButton!
+    @IBOutlet private weak var addMarkerButton: UIButton!
 
     var hrMonitor: HRMonitor!
     var sessionsService: SessionsService!
+    
+    private var state: State = .stopped {
+        didSet {
+            updateInterface(state: state)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,11 +44,39 @@ final class RecordVC: UIViewController {
     }
 
     @IBAction func startButtonPressed(_ sender: UIButton) {
+        state = .recording
+        
         sessionsService.startRecording()
     }
+    
+    @IBAction func pauseButtonPressed(_ sender: UIButton) {
+        state = .paused
+        
+        sessionsService.stopRecording(finish: false)
+    }
 
+    @IBAction func resumeButtonPressed(_ sender: UIButton) {
+        state = .recording
+
+        sessionsService.startRecording()
+    }
+    
     @IBAction func stopButtonPressed(_ sender: UIButton) {
+        state = .stopped
+
         sessionsService.stopRecording(finish: true)
+    }
+
+    @IBAction func addMarkerButtonPressed(_ sender: UIButton) {
+        sessionsService.addMarker()
+    }
+    
+    private func updateInterface(state: State) {
+        startButton.isHidden = state != .stopped
+        pauseButton.isHidden = state != .recording
+        addMarkerButton.isHidden = state != .recording
+        resumeButton.isHidden = state != .paused
+        stopButton.isHidden = state != .paused
     }
 }
 
